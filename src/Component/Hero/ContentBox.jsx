8784a7b74cda129527b469afb98180bd13e3fe51
Pallavi Soldey <pallavisolday12@gmail.com>
2023-09-15 11:09:19 +0530

    add about me in home section

--- src/Component/Hero/ContentBox.jsx
+++ src/Component/Hero/ContentBox.jsx
@@ -6,10 +6,10 @@ const ContentBox = () => {
     return (
         <>
             <div className='contentBox'>
-                <h3 className='name-line'>Hello, my name is <span className='name'>Pallavi Soldey</span></h3>
+                <h3 className='name-line'>Hello, I'm <span className='name'>Pallavi Soldey</span></h3>
                  <AboutContent
-                    head= "I'm a"
-                    para= " your portfolio should be well-organized, visually appealing, and easy to navigate. It's essential to keep it up to date and regularly add new projects and experiences as you progress in your career. Tailor the content to your specific audience and the type of opportunities you seek."
+                    head= "a"
+                    para= "I'm a motivated front-end developer with a passion for learning and collaborating with experienced peers. I specialize in creating user-friendly websites and web applications, proficient in HTML, CSS, JavaScript, ReactJs, NextJs, and much more."
                  />
                 <Button title="Download CV"/>
 
